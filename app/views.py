7a1a65dfe86f7e950db1a831c27273575e103c36
whimo <me@whimo.me>
2018-05-19 21:23:16 +0300

    New repo endpoint

--- app/views.py
+++ app/views.py
@@ -1,9 +1,13 @@
 from app import app
 from flask import render_template, request, abort, jsonify
+import os
+from subprocess import check_output, TimeoutExpired
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 @app.route('/repo_url', methods=['POST'])
 def repo_url():
@@ -11,3 +15,30 @@ def repo_url():
         abort(400)
     print(request.get_json())
     return jsonify({'status': 'SUCC sees'})
+
+
+@app.route('/new_repo', methods=['CUM', 'POST'])
+def new_repo():
+    if not request.json or 'url' not in request.json:
+        return jsonify({'status': 'error', 'error_text': 'You must specify repo\'s .git'}), 400
+
+    repo_url = request.json['url']
+    repo_name = repo_url.split('/')[-1][:-4]
+    repo_name.replace('..', 'ERROR')
+
+    os.system('rm -rf {}'.format(os.path.join(app.config['REPOS_DIR'], repo_name)))
+
+    try:
+        output = check_output(['git', 'clone', repo_url, os.path.join(app.config['REPOS_DIR'], repo_name)],
+                              timeout=app.config['CLONE_TIMEOUT'])
+        output = output.decode()
+
+    except TimeoutExpired:
+        return jsonify({'status': 'error', 'error_text': 'Timeout expired'}), 400
+
+    if 'done' in output:
+        return jsonify({'status': 'ok'}), 201
+    elif 'not found' in output:
+        return jsonify({'status': 'error', 'error_text': 'Repository not found'}), 404
+
+    return jsonify({'status': 'error', 'error_text': 'Unknown error'}), 400
