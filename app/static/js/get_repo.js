1ea54e23bdc44cc739da1494b7eb41dd6841382c
QwertygidQ <Qwertygid@gmail.com>
2018-05-19 13:02:23 +0300

    Started working with Ajax

--- app/static/js/get_repo.js
+++ app/static/js/get_repo.js
@@ -1,6 +1,21 @@
 $(document).ready(function() {
     $('#repo_search').click(function() {
-        alert('click');
+        $.ajax({
+            url: '/test',
+            type: 'POST',
+            contentType: 'application/json',
+            dataType: 'json',
+            data: JSON.stringify({
+                text: 'sosi'
+            }),
+            success: function (response) {
+                alert(response);
+                console.log(response);
+            },
+            error: function () {
+                alert('error');
+            }
+        });
     });
     
     $('#repo_upload').click(function() {
--- app/views.py
+++ app/views.py
@@ -1,6 +1,13 @@
 from app import app
-from flask import render_template
+from flask import render_template, request, abort, jsonify
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/test', methods=['POST'])
+def test():
+    if not request.get_json():
+        abort(400)
+    print(request.get_json())
+    return jsonify({'status': 'SUCC sees'})
