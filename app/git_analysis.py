ef1048b2f8a8790ef64d80d7f093813d5e49cd6b
syn <isaqtm@gmail.com>
2018-05-20 15:18:07 +0300

    Create more functions for repo analysis

--- app/git_analysis.py
+++ app/git_analysis.py
@@ -1,5 +1,6 @@
 from subprocess import check_output, CalledProcessError
 import re
+from datetime import datetime, timedelta
 
 
 def show_author_of_line(file, line_off, line, prev_sha):
@@ -8,7 +9,9 @@ def show_author_of_line(file, line_off, line, prev_sha):
 
     try:
         output = check_output(cmd).decode('utf-8')
-        r_author = r'\(([a-zA-Z0-9 ]*) ([0-9\-\ \:]*\+[0-9]{4,4}) ' + str(line + line_off) + r'\)'
+        r_author = r'\(([a-zA-Z0-9 ]*) ([0-9\-\ \:]*\+[0-9]{4,4}) ' + \
+                   str(line + line_off) + r'\)'
+
         captures = re.search(r_author, output)
         return captures.group(1), captures.group(2)  # author, time
     except CalledProcessError:
@@ -62,13 +65,19 @@ def stats_for_commit(sha1):
 
         if stage == 2:
             line += '\n'  # to match end of line
-            r_path = r'[\-\+]{3,3} a\/([^\0]*)\n'  # e.g. '--- a/path/to/file.ext'
+
+            # e.g. '--- a/path/to/file.ext'
+            r_path = r'[\-\+]{3,3} a\/([^\0]*)\n'
+
             filename = re.search(r_path, line).group(1)
             stage = 0
             continue
 
         if line.startswith('@@'):
-            r_lines = r'\@\@ [\-\+]([0-9]*),[0-9]* [\-\+]([0-9]*),[0-9]* \@\@'  # e.g. '<sha-1> (<name> <date> line)\n'
+
+            # e.g. '<sha-1> (<name> <date> line)\n'
+            r_lines = r'\@\@ [\-\+]([0-9]*),[0-9]* [\-\+]([0-9]*),[0-9]* \@\@'
+
             diff_start = re.search(r_lines, line)
             line_a = int(diff_start.group(1))
             offset = 0
@@ -76,7 +85,6 @@ def stats_for_commit(sha1):
             continue
 
         if line.startswith('diff'):
-            files_in_commit += 1
             stage = 1
             continue
 
@@ -87,8 +95,10 @@ def stats_for_commit(sha1):
                 diffs.append(('+', (None, None)))
 
         elif line.startswith('-') and not line.startswith('---'):
-            diffs.append(('-', show_author_of_line(filename, offset + del_off,
-                                                   line_a, '{}'.format(commit))))
+            diffs.append(('-', show_author_of_line(filename,
+                                                   offset + del_off,
+                                                   line_a,
+                                                   '{}'.format(commit))))
             del_off -= 1
 
         offset += 1
@@ -112,3 +122,130 @@ def get_commits_period(from_date, to_date):
            '--all', '--no-merges']
 
     return check_output(cmd).decode('utf-8').split()
+
+
+ISO8601 = '%Y-%m-%d %H:%M:%S %z'
+
+
+def get_description(sha):
+    cmd = f'git log --format=%B -n 1 {sha}'
+    return check_output(cmd.split()).decode('utf-8')
+
+
+def get_diff_count(sha):
+    cmd = f'git diff {sha}^ {sha} --stat'.split()
+    stat = check_output(cmd).decode('utf-8').split('\n')[:-2]
+
+    r_change = r'.* (|) *([0-9]+).*'
+    changes = 0
+
+    for line in stat:
+        print(repr(line))
+        changes += int(re.search(r_change, line).group(2))
+    return changes
+
+
+def get_time(sha):
+    cmd = f'git log --format=%ai -n 1 {sha}'.split()
+    return check_output(cmd).decode('utf-8').replace('\n', '')
+
+
+def is_deleting_old_code(stats, date):
+    date = datetime.strptime(date, ISO8601) - timedelta(days=30)
+    changed_elder_than_month = 0
+    deletes = 0
+    for stat in stats:
+        if stat[0] == '-':
+            deletes += 1
+            if datetime.strptime(stat[1][1], ISO8601) <= date:
+                changed_elder_than_month += 1
+
+    if float(changed_elder_than_month) / float(deletes) > 0.8:
+        return True
+
+    return False
+
+
+def is_helping_others(stats, me):
+    deletes = 0
+    helps = 0
+    for stat in stats:
+        if stat[0] == '-':
+            deletes += 1
+            if stat[1][0] != me:
+                helps += 1
+
+    if float(helps) / float(deletes) > 0.8:
+        return True
+
+    return False
+
+
+def is_churning(stats, date, me):
+    date = datetime.strptime(date, ISO8601) - timedelta(days=7)
+    changed_newer_than_week = 0
+    deletes = 0
+    for stat in stats:
+        if stat[0] == '-':
+            deletes += 1
+            if datetime.strptime(stat[1][1], ISO8601) >= date:
+                if stat[1][0] == me:
+                    changed_newer_than_week += 1
+
+    if float(changed_newer_than_week) / float(deletes) > 0.8:
+        return True
+
+    return False
+
+
+def get_commit_info(sha):
+    cmd = ['git', 'log', '--format=%an', '-n', '1', sha]
+    me = check_output(cmd).decode('utf-8').replace('\n', '')
+
+    commit_stats = stats_for_commit(sha)
+
+    minus = 0
+    add = 0
+    change = 0
+    total = len(commit_stats)
+    for stat in commit_stats:
+        if stat[0] == '+':
+            add += 1
+        elif stat[0] == '-':
+            minus += 1
+        else:
+            change += 1
+
+    if float(add + change) / float(total) > 0.8:
+        c_type = 'New Work'
+
+    elif is_deleting_old_code(commit_stats, get_time(sha)):
+        c_type = 'Refactoring'
+
+    elif is_helping_others(commit_stats, me):
+        c_type = 'Helping Others'
+
+    elif is_churning(commit_stats, get_time(sha), me):
+        c_type = 'Code Churn'
+
+    else:
+        c_type = 'Other'
+
+    get_files = f'git diff {sha}^ {sha} --name-only'.split()
+    files = check_output(get_files).decode('utf-8').split('\n')[:-1]
+
+    return c_type, files
+
+
+def get_counts_period(from_date, to_date):
+    commits = get_commits_period(from_date, to_date)
+    activity = {'New Work': 0,
+                'Refactoring': 0,
+                'Helping Others': 0,
+                'Code Churn': 0}
+    for commit in commits:
+        c_type, _ = get_commit_info(commit)
+        amount = get_diff_count(commit)
+        activity[c_type] += amount
+
+    return activity
